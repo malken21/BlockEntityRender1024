38a28d1283657a6d44fc8b89b00ba2c99167987b
malken21 <Malken_Marumasa@outlook.jp>
2023-09-16 04:49:48 +0900

    add Logger

--- src/main/java/marumasa/aaa_block_entity_render1024/BlockEntityRender1024.java
+++ src/main/java/marumasa/aaa_block_entity_render1024/BlockEntityRender1024.java
@@ -1,9 +1,15 @@
 package marumasa.aaa_block_entity_render1024;
 
+import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
 
 public class BlockEntityRender1024 implements ModInitializer {
+    public static final String MOD_ID = "aaa_block_entity_render1024";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
     @Override
     public void onInitialize() {
+        LOGGER.info("Start: " + MOD_ID);
     }
 }
